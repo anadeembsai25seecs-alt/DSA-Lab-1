581b534c69cdc1a56f4e49072f362c54fdb47c20
Abeera Nadeem <anadeem.bsai25seecs@seecs.edu.pk>
2026-09-09 12:51:06 +0500

    Add smoke test for utilities::add

--- test_utilities.cpp
+++ test_utilities.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 int main() {
     cout << "add(2,3) == 5: " << (add(2, 3) == 5 ? "PASS" : "FAIL") << endl;
+
+    cout << "add(9,0) == 9: " << (add(9, 0) == 9 ? "PASS" : "FAIL") << endl;
+
+    cout << "add(10,89) == 199: " << (add(10, 89) == 199 ? "PASS" : "FAIL") << endl;
+
     return 0;
 }
